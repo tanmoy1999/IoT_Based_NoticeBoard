98a72a28a4000a74838ee86582eee5d0008c149c
Tanmoy Munshi <42179859+tanmoy1999@users.noreply.github.com>
2020-03-22 19:23:41 +0530

    Create noticeBoard.ino

---
+++ IoT_noticeBoard/noticeBoard.ino
@@ -0,0 +1,81 @@
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <Wire.h>
+#include<LCD.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+
+AsyncWebServer server(80);
+
+const char* ssid = "D-Link_DIR-600M";  //wifi ssid
+const char* password = "alliswell";   //wifi password
+
+const char* PARAM_INPUT_1 = "input1";
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html><head>
+  <title>Smart Notice Board</title>
+  <meta name="viewport" content="width=device-width, initial-scale=5">
+<p> <font size="9" face="sans-serif"> <marquee> Smart Notice Board </marquee> </font> </p>
+  </head><body><center>
+  <form action="/get">
+    Enter Text to Display: <input type="text" name="input1">
+    <input type="submit" value="Send">
+  </form><br>
+ 
+</center></body></html>)rawliteral";
+
+void notFound(AsyncWebServerRequest *request) {
+  request->send(404, "text/plain", "Not found");
+}
+
+void setup() {
+  Serial.begin(115200);
+   lcd.begin(16, 2);
+   lcd.clear();
+   lcd.setCursor(0, 0);
+   lcd.print("Smart Notice Board");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("WiFi Failed!");
+    return;
+  }
+  Serial.println();
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html);
+  });
+
+  server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
+    String message;
+    String inputParam;
+    if (request->hasParam(PARAM_INPUT_1)) {
+      message = request->getParam(PARAM_INPUT_1)->value();
+      inputParam = PARAM_INPUT_1;
+       lcd.clear();
+       lcd.setCursor(16,0);
+      
+      lcd.print(message);
+    }
+    else {
+      message = "No message sent";
+      inputParam = "none";
+    }
+    Serial.println(message);
+   
+  request->send(200, "text/html", index_html);
+  });
+  server.onNotFound(notFound);
+  server.begin();
+}
+
+void loop() {
+   
+    lcd.scrollDisplayLeft();
+    delay(500);
+  
+}
